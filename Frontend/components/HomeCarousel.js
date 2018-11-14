ce1c7a10759923e9926e97a79c44a61fd2d995d6
willlyprkl <vili.korhonen@hotmail.fi>
2018-11-14 18:46:46 +0200

    HelAPI merge

--- Frontend/components/HomeCarousel.js
+++ Frontend/components/HomeCarousel.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import {View, Text, Image, ScrollView} from 'react-native';
 import CarouselCard from './CarouselCard';
-
-var imgurl = "";
-var title = "";
-
-// Fetching data from city's API
-fetch('https://linkedevents-api.test.hel.ninja/linkedevents-test/v1/event/?start=2018-11-14', {
-  method: 'GET',
-  headers: {
-    'api_key': '934d7aed-9929-4b55-b70f-b60e4772316d',
-  },
-})
-.then(response => response.json())
-.then(data => setCarousel(data))
-.catch(error => console.log("JSON ERROR"));
+import * as HelAPI from '../scripts/HelAPI';
 
 
-function setCarousel(datax) {
+async function setCarousel() {
+  var datax = await HelAPI.getTodaysEvents();
   console.log("Datax: " + datax);
-  console.log("Data name(fi): " + datax.data[1].name.fi);
-  console.log("Data image url: " + datax.data[1].images[0].url);
+  console.log("Event name(fi): " + datax.data[1].name.fi);
+  console.log("Event image url: " + datax.data[1].images[0].url);
+  return datax;
 }
+
+var datax = setCarousel();
 
 export default class HomeCarousel extends React.Component {
   constructor(props){
--- Frontend/scripts/HelAPI.js
+++ Frontend/scripts/HelAPI.js
@@ -1,187 +1,225 @@
 
+
 // Url and param
 var apiUrl = "https://linkedevents-api.test.hel.ninja/linkedevents-test/v1/";
 var apiKey = "934d7aed-9929-4b55-b70f-b60e4772316d"
 
 
 // Get all events
-function getAllEvents() {
-  return fetch(apiUrl + "event/", {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
-}
-
-
-// Get events by date
-function getEventsByDate(startDate, endDate) {
-  //var d = new Date();
-  //var n = "" + d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
-  return fetch(apiUrl + "event/?start=" + startDate + "&end=" + endDate, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
-}
-
-
-// Get todays EventScreen
-function getTodaysEvents() {
-  return fetch(apiUrl + "event/?start=today&end=today", {
+export async function getAllEvents() {
+  try {
+    let response = await fetch(apiUrl + "event/", {
       method: 'GET',
       headers: {
         "api_key": apiKey,
         "Content-type": "application/json"
       },
-    })
-    .then(response => response.json())
-    .then(data => {return data})
-    .catch(error => console.log("JSON ERROR"));
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+// Get events by date
+export async function getEventsByDate(startDate, endDate) {
+  //var d = new Date();
+  //var n = "" + d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate();
+  try {
+    let response = await fetch(apiUrl + "event/?start=" + startDate + "&end=" + endDate, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+// Get todays EventScreen
+export async function getTodaysEvents() {
+  try {
+    let response = await fetch(apiUrl + "event/?start=today&end=today", {
+        method: 'GET',
+        headers: {
+          "api_key": apiKey,
+          "Content-type": "application/json"
+        },
+      });
+      let responseJson = await response.json();
+      return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+
 }
 
 
 // Get event by Text
-function getEventsByText(text) {
-  return fetch(apiUrl + "event/?text=" + text, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getEventsByText(text) {
+  try {
+    let response = await fetch(apiUrl + "event/?text=" + text, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // Get events by location
-function getEventsByLocation(location) {
-  return fetch(apiUrl + "event/?location=" + location, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getEventsByLocation(location) {
+  try {
+    let response = await fetch(apiUrl + "event/?location=" + location, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // Get events by division
-function getEventsByDivision(div) {
-  return fetch(apiUrl + "event/?location=" + div, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getEventsByDivision(div) {
+  try {
+    let response = await fetch(apiUrl + "event/?location=" + div, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 
 // Get event by keywords
-function getEventsByKeyword(keyword) {
-  return fetch(apiUrl + "event/?keyword=" + keyword, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getEventsByKeyword(keyword) {
+  try {
+    let response = await fetch(apiUrl + "event/?keyword=" + keyword, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // Get an event by id
-function getEventById(eventId) {
-  return fetch(apiUrl + "event/" + eventId, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getEventById(eventId) {
+  try {
+    let response = await fetch(apiUrl + "event/" + eventId, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // Get all keywords
-function getAllKeywords() {
-  return fetch(apiUrl + "keyword/", {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getAllKeywords() {
+  try {
+    let response = await fetch(apiUrl + "keyword/", {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // Get keyword by id
-function getAllKeywordsById(keywordId) {
-  return fetch(apiUrl + "keyword/" + keywordId, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function getAllKeywordsById(keywordId) {
+  try {
+    let response = await fetch(apiUrl + "keyword/" + keywordId, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // Event search
-function searchEvent(search) {
-  return fetch(apiUrl + "search/?type=event&q=" + search, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-      "Content-type": "application/json"
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function searchEvent(search) {
+  try {
+    let response = await fetch(apiUrl + "search/?type=event&q=" + search, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+        "Content-type": "application/json"
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
 // !!!!!!!!!!!!!!! NOT WORKING !!!!!!!!!!!!!!!
 // Search autocomplete
-function searchAutocomplete(search, type) {
-  return fetch(apiUrl + "search/?type=" + type + "&input=" + search, {
-    method: 'GET',
-    headers: {
-      "api_key": apiKey,
-    },
-  })
-  .then(response => response.json())
-  .then(data => {return data})
-  .catch(error => console.log("JSON ERROR"));
+export async function searchAutocomplete(search, type) {
+  try {
+    let response = await fetch(apiUrl + "search/?type=" + type + "&input=" + search, {
+      method: 'GET',
+      headers: {
+        "api_key": apiKey,
+      },
+    });
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
 }
