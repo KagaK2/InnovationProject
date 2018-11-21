54bea9ffe79bd01027ad5953ab4ee6337f0100a2
willlyprkl <vili.korhonen@hotmail.fi>
2018-11-21 23:03:10 +0200

    Basic DB functions
    User and event stuff.

--- Frontend/scripts/FBcon.js
+++ Frontend/scripts/FBcon.js
@@ -1,20 +1,80 @@
-import Firebase from 'firebase';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyAMFtCMKracMLES-ddu4P3h4RamNN8lCoA",
-  authDomain: "gosinki-ee.firebaseapp.com",
-  databaseURL: "https://gosinki-ee.firebaseio.com",
-  projectId: "gosinki-ee",
-  storageBucket: "gosinki-ee.appspot.com",
-  messagingSenderId: "718950483191"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
-Firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-function writeUserData(userId, name) {
-  Firebase.database().ref('Users/' + userId).set({
-    id: userId,
-    name: name,
-  });
+const db = firebase.firestore();
+db.settings({timestampsInSnapshots: true});
+
+const usersRef = db.collection('users');
+const eventsRef = db.collection('events');
+
+var user = "";
+
+// USE AFTER LOGIN
+// Passes the information to the database and checks if the user already
+// exists.
+export async function checkUser(userId, name) {
+    usersRef.doc(userId).get()
+      .then((docSnapshot) => {
+        if(docSnapshot.exists) {
+          usersRef.onSnapshot((doc) => {
+            console.log("User found");
+            user = userId;
+          });
+        } else {
+          console.log("User not found");
+          var data = {
+            id: userId,
+            name: name,
+          }
+          usersRef.doc(userId).set(data);
+          user = userId;
+        }
+      })
+}
+
+
+// USE AFTER CLICKING 'JOIN EVENT'
+// Checks if the event is 'marked', either creates the event info or adds the
+// user to it.
+export async function checkEvent(eventId, eventName, eventDate, eventDesc) {
+    eventsRef.doc(eventId).get()
+      .then((docSnapshot) => {
+        if(docSnapshot.exists) {
+          eventsRef.onSnapshot((doc) => {
+            console.log("Event found");
+            addUserToEvent(user, eventId);
+          });
+        } else {
+          console.log("Event not found");
+          var data = {
+            id: eventId,
+            name: eventName,
+            date: eventDate,
+            desc: eventDesc,
+            expired: false,
+          }
+          eventsRef.doc(eventId).set(data);
+          addUserToEvent(user, eventId);
+        }
+      })
+}
+
+export async function addUserToEvent(blast, event) {
+    eventsRef.doc(event).update({attendees: firebase.firestore.FieldValue.arrayUnion(blast)});
+}
+
+export async function removeUserFromEvent(blast, event) {
+    eventsRef.doc(event).update({attendees: firebase.firestore.FieldValue.arrayRemove(blast)});
 }
