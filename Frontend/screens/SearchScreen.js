78733803d9b2e4b7268eabaf45a4c18daa2597d9
quangnhat224 <quangnhat224@gmail.com>
2018-11-26 18:22:06 +0200

    adding search screen and discussion screen.

--- Frontend/screens/SearchScreen.js
+++ Frontend/screens/SearchScreen.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import {View, ScrollView, Text, TextInput} from 'react-native';
+import {View, ScrollView, Text, TextInput, TouchableOpacity, Image} from 'react-native';
+import {Styles} from '../styles/componentstyle.js';
+import SearchBar from '../components/SearchBar.js';
+import { connect } from 'react-redux';
 
-export default class SearchScreen extends React.Component {
+class SearchScreen extends React.Component {
   constructor(props){
     super(props);
   }
   static navigationOptions = { header: null };
   render(){
     return(
-      <View>
-        <View>
-          <TextInput value=''/>
+      <ScrollView style={Styles.colorBody}>
+        <View style={Styles.appBody}>
+          <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View>
+              <Text style={Styles.headline}>Search</Text>
+              <Text style={Styles.largeBody}>This is the search screen.</Text>
+            </View>
+            <TouchableOpacity style={Styles.profileIconContainer}>
+              <Image
+                style={Styles.profileIcon}
+                source={{uri: this.props.picurl}}
+              />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <SearchBar/>
+          </View>
         </View>
-        <ScrollView>
-          
-        </ScrollView>
-      </View>
+      </ScrollView>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    picurl: state.reducer.picurl,
+  };
+}
+
+export default connect(mapStateToProps)(SearchScreen);
